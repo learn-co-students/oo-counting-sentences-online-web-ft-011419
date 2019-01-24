044c5ed44fe833f16396c9446bca851ba71f9320
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-24 19:51:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,10 +19,17 @@ class String
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
+
+
+
