51bb9fe14b2ffef6ed627d268bae86b77ecdf547
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-24 18:41:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    elsif !self.end_with?(".")
+      return false
+    end  
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    elsif !self.end_with?("?")
+      return false
+    end
   end
 
   def exclamation?
